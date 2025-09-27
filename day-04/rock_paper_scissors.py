96ac3554f0bb885e23109fd00ea56ef5bc46b79f
cheddar <gr00.y3re@gmail.com>
2025-09-27 15:51:40 +0200

    input validation added in day 4 exercise

--- day-04/rock_paper_scissors.py
+++ day-04/rock_paper_scissors.py
@@ -5,17 +5,19 @@ computer_choose = random.randint(0, 2)
 
 choices = ["Rock", "Paper", "Scissors"]
 
-print(f"You've chosen {choices[player_input]}")
-print(f"Computer chose {choices[computer_choose]}")
-
-if computer_choose == 0 and player_input == 2:
-    print("You lost!")
-elif player_input == 0 and computer_choose == 2:
-    print("You won!")
+if player_input >= 3:
+    print("You've chosen wrong number. Try again!")
 else:
-    if player_input > computer_choose:
-        print("You won!")
-    elif player_input == computer_choose:
-        print("It's a draw!")
-    else:
+    print(f"You've chosen {choices[player_input]}")
+    print(f"Computer chose {choices[computer_choose]}")
+    if computer_choose == 0 and player_input == 2:
         print("You lost!")
+    elif player_input == 0 and computer_choose == 2:
+        print("You won!")
+    else:
+        if player_input > computer_choose:
+            print("You won!")
+        elif player_input == computer_choose:
+            print("It's a draw!")
+        else:
+            print("You lost!")
