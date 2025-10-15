65f08d221130a4ed45655679ec4878efe7e59006
cheddar <gr00.y3re@gmail.com>
2025-10-15 21:50:39 +0200

    variables names unification

--- day-10/calculator.py
+++ day-10/calculator.py
@@ -3,19 +3,19 @@ from functions import add, subtract, multiply, divide, calculations
 
 def calculator():
     print(logo)
-    num1 = float(input("What's the first number?: "))
+    n1 = float(input("What's the first number?: "))
     for key in calculations:
         print(key)
     running = True
 
     while running:
         operation = input("Pick an operation: ")
-        num2 = float(input("What's the next number?: "))
+        n2 = float(input("What's the next number?: "))
         task = calculations[operation]
-        answer = task(num1, num2)
-        print(f"{num1} {operation} {num2} = {answer}")
+        answer = task(n1, n2)
+        print(f"{n1} {operation} {n2} = {answer}")
 
-        continuity = input(f"Type 'y' to continue calculating with {num2}, or type 'n' to start a new calculation.: ")
+        continuity = input(f"Type 'y' to continue calculating with {n2}, or type 'n' to start a new calculation.: ")
 
         if  continuity == 'y':
             num1 = answer
